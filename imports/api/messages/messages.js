e5aeb827be6e309ccb49cc7da0ebf56a3ef2d456
cristianoroch56 <cristiano.ecombiz@gmail.com>
2021-04-12 21:29:15 +0530

    Latest fixes

--- imports/api/messages/messages.js
+++ imports/api/messages/messages.js
@@ -2,8 +2,4 @@ import { Mongo } from "meteor/mongo";
 
 const Messages = new Mongo.Collection("messages");
 
-Messages.remove({});
-
-// console.log(Messages.find().fetch())
-
 export default Messages;
